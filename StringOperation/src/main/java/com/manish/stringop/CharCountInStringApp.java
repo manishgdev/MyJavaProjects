c3323d5bb61e5571cdb91378b1f2daf74676f6ec
manishgdev <manishgdev@gmail.com>
2016-06-13 07:42:52 +0530

    Commiting StringOperation Project which will contain code related to String manipulation

---
+++ StringOperation/src/main/java/com/manish/stringop/CharCountInStringApp.java
@@ -0,0 +1,36 @@
+package com.manish.stringop;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.ArrayList;
+/**
+ * Hello world!
+ *
+ */
+public class CharCountInStringApp 
+{
+    public static void main( String[] args )
+    {
+        String myString = "Cucumber Execution Test";
+        myString = myString.toUpperCase();
+        System.out.println("String to test is : '"+myString+"'");
+        LinkedHashMap<String, Integer> charCountMap = new LinkedHashMap<String, Integer>();
+        
+        for(int i=0; i<myString.length(); i++) {
+			char c = myString.charAt(i);
+			char chArr[] = new char[]{c};
+			String tmpString = new String(chArr);
+			
+			// System.out.println("The Character at ("+i+") position is '"+tmpString+"'");
+			if(charCountMap.get(tmpString) != null) {
+				Integer strCount = charCountMap.get(tmpString);
+				// strCount++;
+				charCountMap.put(tmpString, ++strCount);
+			}
+			else {
+				charCountMap.put(tmpString, new Integer(1));
+			}
+		}
+		System.out.println("Character Count Map : "+charCountMap);
+    }
+}
